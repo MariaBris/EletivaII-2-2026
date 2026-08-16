3d35ea6073432041c53706deefca4bc8b0654d9d
MariaBris <nanda.sanches.bris@gmail.com>
2026-08-16 10:27:37 -0300

    exercicios

---
+++ Exercicios/app/Http/Controllers/ExerciciosController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExerciciosController extends Controller
+{
+    public function abrirFormExer1(){
+        return view('exer1');
+    }
+
+    public function respostaExer1(Request $request){
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $soma = $valor1 + $valor2;
+        return view('exer1', ['soma' => $soma]);
+    }
+
+    public function abrirFormExer2(){
+        return view('exer2');
+    }
+
+    public function respostaExer2(Request $request){
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $sub = $valor1 - $valor2;
+        return view('exer2', ['sub' => $sub]);
+    }
+
+    public function abrirFormExer3(){
+        return view('exer3');
+    }
+
+    public function respostaExer3(Request $request){
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $mult = $valor1 * $valor2;
+        return view('exer3', ['mult' => $mult]);
+    }
+}
--- Exercicios/routes/web.php
+++ Exercicios/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExerciciosController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/exer1', [ExerciciosController::class, 'abrirFormExer1']);
+
+Route::post('exer1resp', [ExerciciosController::class, 'respostaExer1']);
+
+Route::get('/exer2', [ExerciciosController::class, 'abrirFormExer2']);
+
+Route::post('exer2resp', [ExerciciosController::class, 'respostaExer2']);
+
+Route::get('/exer3', [ExerciciosController::class, 'abrirFormExer3']);
+
+Route::post('exer3resp', [ExerciciosController::class, 'respostaExer3']);
